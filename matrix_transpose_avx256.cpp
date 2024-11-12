f6221a14a3697288877a8febc398ae277c57b06a
fischeri230503 <417masaya@gmail.com>
2024-11-13 00:23:39 +0900

    main bubun sakusei

--- matrix_transpose_avx256.cpp
+++ matrix_transpose_avx256.cpp
@@ -1,64 +1,10 @@
 #include <algorithm>
+//
 #include <cmath>
 #include <cstddef>
 #include <cstdint>
+#include <cstdlib>
 #include <immintrin.h>
-
-// original https://qiita.com/beru/items/12b4249c95a012a28ccd
-template <typename T>
-inline void transpose(size_t width, size_t height, const T *__restrict pSrc, ptrdiff_t srcLineStride,
-                      T *__restrict pDst, ptrdiff_t dstLineStride) noexcept
-{
-    if (width == 0 || height == 0 || pSrc == nullptr || pDst == nullptr || srcLineStride < width ||
-        dstLineStride < height)
-    {
-        return;
-    }
-
-    if (width >= 16 / sizeof(T) && height >= 16)
-    {
-        size_t bw = std::min<size_t>(128u / sizeof(T), flp2(width));
-        size_t bh = std::min<size_t>(512u / sizeof(T), flp2(height));
-        size_t nw = width / bw;
-        size_t nh = height / bh;
-        for (size_t i = 0; i < nh; ++i)
-        {
-            for (size_t j = 0; j < nw; ++j)
-            {
-                auto pSrc2 = &pSrc[i * bh * srcLineStride + j * bw];
-                auto pDst2 = &pDst[j * bw * dstLineStride + i * bh];
-                constexpr size_t n = 16;
-                for (size_t l = 0; l < bh / n; ++l)
-                {
-                    for (size_t k = 0; k < bw / n; ++k)
-                    {
-                        auto pSrc3 = &pSrc2[l * n * srcLineStride + k * n];
-                        auto pDst3 = &pDst2[k * n * dstLineStride + l * n];
-                        transpose_1b_16x16(pSrc3, srcLineStride, pDst3, dstLineStride);
-                    }
-                }
-            }
-        }
-        size_t widthRemain = width % bw;
-        size_t heightRemain = height % bh;
-        size_t width2 = bw * nw;
-        size_t height2 = bh * nh;
-        if (widthRemain)
-        {
-            transpose(widthRemain, height2, pSrc + width2, srcLineStride, pDst + (width2 * dstLineStride),
-                      dstLineStride);
-        }
-        if (heightRemain)
-        {
-            transpose(width, heightRemain, pSrc + height2 * srcLineStride, srcLineStride, pDst + height2,
-                      dstLineStride);
-        }
-    }
-    else
-    {
-        transpose_naive(width, height, pSrc, srcLineStride, pDst, dstLineStride);
-    }
-}
 
 // https://stackoverflow.com/a/2681094
 inline uint32_t flp2(uint32_t x)
@@ -144,4 +90,88 @@ inline void transpose_1b_16x16(const uint8_t *__restrict pSrc, ptrdiff_t srcLine
     b7 = _mm256_unpackhi_epi32(a6, a7);
     a7 = _mm256_permute4x64_epi64(b7, _MM_SHUFFLE(3, 1, 2, 0));
     _mm256_storeu2_m128i((__m128i *)(pDst + 15 * dstLineStride), (__m128i *)(pDst + 14 * dstLineStride), a7);
+}
+
+void transpose_naive(size_t columns, size_t rows, const int8_t *__restrict pSrc, ptrdiff_t srcLineStride, int8_t *__restrict pDst,
+                     ptrdiff_t dstLineStride) noexcept
+{
+    for (size_t x = 0; x < columns; ++x)
+    {
+        for (size_t y = 0; y < rows; ++y)
+        {
+            pDst[(x * dstLineStride) + y] = pSrc[(y * srcLineStride) + x];
+        }
+    }
+}
+
+// original https://qiita.com/beru/items/12b4249c95a012a28ccd
+inline void transpose(size_t width, size_t height, const int8_t *__restrict pSrc, ptrdiff_t srcLineStride,
+                      int8_t *__restrict pDst, ptrdiff_t dstLineStride) noexcept
+{
+    if (width == 0 || height == 0 || pSrc == nullptr || pDst == nullptr || srcLineStride < width ||
+        dstLineStride < height)
+    {
+        return;
+    }
+
+    if (width >= 16 / sizeof(int8_t) && height >= 16)
+    {
+        size_t bw = std::min<size_t>(128u / sizeof(int8_t), flp2(width));
+        size_t bh = std::min<size_t>(512u / sizeof(int8_t), flp2(height));
+        size_t nw = width / bw;
+        size_t nh = height / bh;
+        for (size_t i = 0; i < nh; ++i)
+        {
+            for (size_t j = 0; j < nw; ++j)
+            {
+                auto pSrc2 = &pSrc[i * bh * srcLineStride + j * bw];
+                auto pDst2 = &pDst[j * bw * dstLineStride + i * bh];
+                constexpr size_t n = 16;
+                for (size_t l = 0; l < bh / n; ++l)
+                {
+                    for (size_t k = 0; k < bw / n; ++k)
+                    {
+                        auto pSrc3 = &pSrc2[l * n * srcLineStride + k * n];
+                        auto pDst3 = &pDst2[k * n * dstLineStride + l * n];
+                        transpose_1b_16x16(pSrc3, srcLineStride, pDst3, dstLineStride);
+                    }
+                }
+            }
+        }
+        size_t widthRemain = width % bw;
+        size_t heightRemain = height % bh;
+        size_t width2 = bw * nw;
+        size_t height2 = bh * nh;
+        if (widthRemain)
+        {
+            transpose(widthRemain, height2, pSrc + width2, srcLineStride, pDst + (width2 * dstLineStride),
+                      dstLineStride);
+        }
+        if (heightRemain)
+        {
+            transpose(width, heightRemain, pSrc + height2 * srcLineStride, srcLineStride, pDst + height2,
+                      dstLineStride);
+        }
+    }
+    else
+    {
+        transpose_naive(width, height, pSrc, srcLineStride, pDst, dstLineStride);
+    }
+}
+
+int main()
+{
+    const size_t width = 256;
+    const size_t height = width;
+    const size_t padding = 128;
+    // const size_t rep = 7;
+    // const size_t bufferSize = (height * std::pow(2, rep) + padding) * (height * std::pow(2, rep) + padding);
+    size_t srcLineStride = width + padding;
+    size_t dstLineStride = height + padding;
+    const int32_t align_number = 64;
+    int8_t *a = (int8_t *)aligned_alloc(align_number, width * height);
+    int8_t *trans_a = (int8_t *)aligned_alloc(align_number, width * height);
+    transpose(width, height, a, srcLineStride, trans_a, dstLineStride);
+
+    return 0;
 }
