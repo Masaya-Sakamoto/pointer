f132e5b78af6a35b0e1ac17b4d2a23965d434300
Masaya-Sakamoto <masaya-sakamoto@kke.co.jp>
2025-03-06 12:13:26 +0900

    保存

--- cublas/simple_cublas_gemm/sample_cublas_sgemm.cu
+++ cublas/simple_cublas_gemm/sample_cublas_sgemm.cu
@@ -1,14 +1,18 @@
-#include <cblas.h>
 #include <chrono>
 #include <cstdlib>
 #include <cublas_v2.h>
 #include <iostream>
 
-// #define DEBUG
+#define DEBUG
+#define CBLAS
 
-#define M 8000
+#ifdef CBLAS
+#include <cblas.h>
+#endif
+
+#define M 2
 #define N 30720
-#define K 30000
+#define K 304
 #define ALIGN 64
 
 void initArray(size_t elements, float *array)
@@ -34,32 +38,41 @@ void print_matrix(const float *A, int rows, int cols)
 
 int main()
 {
-    float *A, *B, *C, *h_C;
-    float *warmupA, *warmupB, *warmupC, *h_warmupC;
+    float *A, *B, *C;
+    float *h_A, *h_B, *h_C;
+
+    // alloc main memory
     A = (float *)aligned_alloc(ALIGN, M * K * sizeof(float));
     B = (float *)aligned_alloc(ALIGN, K * N * sizeof(float));
     C = (float *)aligned_alloc(ALIGN, M * N * sizeof(float));
-    h_C = (float *)aligned_alloc(ALIGN, M * N * sizeof(float)); // for dump
-    warmupA = (float *)aligned_alloc(ALIGN, M * K * sizeof(float));
-    warmupB = (float *)aligned_alloc(ALIGN, K * N * sizeof(float));
-    warmupC = (float *)aligned_alloc(ALIGN, M * N * sizeof(float));
-    h_warmupC = (float *)aligned_alloc(ALIGN, M * N * sizeof(float)); // for dump
+
+    // alloc host memory for fast cudaMemcpy
+    cudaHostAlloc((void **)&h_A, M * K * sizeof(float), cudaHostAllocDefault);
+    cudaHostAlloc((void **)&h_B, K * N * sizeof(float), cudaHostAllocDefault);
+    cudaHostAlloc((void **)&h_C, M * N * sizeof(float), cudaHostAllocDefault);
+
     // Initialize array
     initArray(M * K, A);
     initArray(K * N, B);
-    initArray(M * K, warmupA);
-    initArray(K * N, warmupB);
-    // initArray(M * N, C);
-    // initArray(M * N, h_C);
-    memset(C, 0, M * N * sizeof(float));
-    memset(h_C, 0, M * N * sizeof(float));
-    memset(warmupC, 0, M * N * sizeof(float));
-    memset(h_warmupC, 0, M * N * sizeof(float));
+    initArray(M * N, C);
+
+    // memcpy X -> h_X
+    memcpy(h_A, A, M * K * sizeof(float));
+    memcpy(h_B, B, K * N * sizeof(float));
+    memcpy(h_C, C, M * N * sizeof(float));
 
     // gpu initialization
     // Initialize cuBLAS context.
     cublasHandle_t handle;
     cublasCreate(&handle);
+    cudaStream_t stream;
+    cudaStreamCreate(&stream);
+    cublasSetStream(handle, stream);
+
+    // After creating the stream, set it for cuBLAS
+    cublasCreate(&handle);
+    cublasSetStream(handle, stream); // Add this line
+
     // memcpy to device
     float *d_A, *d_B, *d_C;
     const float d_alpha = 1.0f;
@@ -70,11 +83,14 @@ int main()
 
     // gpu warm up run
     // cublasSetMatrix(); // 　cudamemcpyの代わりにこれを使うと良いかも
-    cudaMemcpy(d_A, warmupA, M * K * sizeof(float), cudaMemcpyHostToDevice);
-    cudaMemcpy(d_C, h_warmupC, M * N * sizeof(float), cudaMemcpyHostToDevice);
-    cudaMemcpy(d_B, warmupB, K * N * sizeof(float), cudaMemcpyHostToDevice);
+    cudaMemcpyAsync(d_A, h_A, M * K * sizeof(float), cudaMemcpyHostToDevice, stream);
+    cudaMemcpyAsync(d_C, h_C, M * N * sizeof(float), cudaMemcpyHostToDevice, stream);
+
+    cudaMemcpyAsync(d_B, h_B, K * N * sizeof(float), cudaMemcpyHostToDevice, stream);
     cublasSgemm(handle, CUBLAS_OP_N, CUBLAS_OP_N, N, M, K, &d_alpha, d_B, N, d_A, K, &d_beta, d_C, N);
-    cudaMemcpy(h_warmupC, d_C, M * N * sizeof(float), cudaMemcpyDeviceToHost);
+    // Synchronize the stream to wait for SGEMM to complete
+    cudaStreamSynchronize(stream);
+    cudaMemcpy(h_C, d_C, M * N * sizeof(float), cudaMemcpyDeviceToHost);
 
 #ifdef DEBUG
     std::cout << "CPU: calculation";
@@ -89,19 +105,35 @@ int main()
     auto count = std::chrono::duration_cast<std::chrono::microseconds>(duration).count();
     std::cout << " done!  " << count << " micro seconds" << std::endl;
 #endif
+    // reinitialize A, B, C
+    initArray(M * K, A);
+    initArray(K * N, B);
+    initArray(M * N, C);
+    memcpy(h_A, A, M * K * sizeof(float));
+    memcpy(h_B, B, K * N * sizeof(float));
+    memcpy(h_C, C, M * N * sizeof(float));
+    cudaMemcpyAsync(d_A, A, M * K * sizeof(float), cudaMemcpyHostToDevice, stream);
+    cudaMemcpyAsync(d_C, h_C, M * N * sizeof(float), cudaMemcpyHostToDevice, stream);
 
+// GPU timing section
 #ifdef DEBUG
     std::cout << "GPU: calculation";
     fflush(stdout);
     auto time_point_gpu = std::chrono::high_resolution_clock::now();
 #endif
-    cudaMemcpyAsync(d_A, A, M * K * sizeof(float), cudaMemcpyHostToDevice);
-    cudaMemcpyAsync(d_C, h_C, M * N * sizeof(float), cudaMemcpyHostToDevice);
-    cudaMemcpyAsync(d_B, B, K * N * sizeof(float), cudaMemcpyHostToDevice);
+
+    // Correct cuBLAS SGEMM call for row-major matrices
+    cudaMemcpyAsync(d_B, h_B, K * N * sizeof(float), cudaMemcpyHostToDevice, stream);
     cublasSgemm(handle, CUBLAS_OP_N, CUBLAS_OP_N, N, M, K, &d_alpha, d_B, N, d_A, K, &d_beta, d_C, N);
-    // memcpy to host
-    cudaMemcpy(h_C, d_C, M * N * sizeof(float), cudaMemcpyDeviceToHost);
+
+    // Synchronize the stream to wait for SGEMM to complete
+    cudaStreamSynchronize(stream);
+
+    // Memcpy to host (this is blocking but timed after SGEMM)
+    cudaMemcpyAsync(h_C, d_C, M * N * sizeof(float), cudaMemcpyDeviceToHost, stream);
+
 #ifdef DEBUG
+    cudaStreamSynchronize(stream); // Ensure all GPU operations are done
     auto duration_gpu = std::chrono::high_resolution_clock::now() - time_point_gpu;
     auto count_gpu = std::chrono::duration_cast<std::chrono::microseconds>(duration_gpu).count();
     std::cout << " done!  " << count_gpu << " micro seconds" << std::endl;
@@ -128,10 +160,14 @@ int main()
     cudaFree(d_C);
 
     // free host memory
+    cudaFreeHost(h_A);
+    cudaFreeHost(h_B);
+    cudaFreeHost(h_C);
+
+    // free main memory
     free(A);
     free(B);
     free(C);
-    free(h_C);
 
     return 0;
 }
